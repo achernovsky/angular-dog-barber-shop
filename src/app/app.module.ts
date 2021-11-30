8a63eee679a1e23caa0161f62e8c79c1834ae5de
Alex Chernovsky <alexche8888@gmail.com>
2021-11-30 13:48:32 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AppointmentsMainComponent } from './components/appointments-main/appointments-main.component';
+import { AppointmentsListComponent } from './components/appointments-list/appointments-list.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AppointmentsMainComponent,
+    AppointmentsListComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/appointments-main/appointments-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointments-main',
+  templateUrl: './appointments-main.component.html',
+  styleUrls: ['./appointments-main.component.css']
+})
+export class AppointmentsMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
