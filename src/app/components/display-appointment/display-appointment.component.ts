d205fb7fa9cae006c845c2e05dba3ffcfa01fbf8
Alex Chernovsky <alexche8888@gmail.com>
2021-12-06 14:07:46 +0200

    fix appointment modal

--- src/app/components/display-appointment/display-appointment.component.ts
+++ src/app/components/display-appointment/display-appointment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Appointment } from 'src/app/models/appointment.model';
 import { AppointmentService } from 'src/app/services/appointment.service';
@@ -8,6 +8,7 @@ import { AppointmentService } from 'src/app/services/appointment.service';
   templateUrl: './display-appointment.component.html',
   styleUrls: ['./display-appointment.component.css']
 })
+
 export class DisplayAppointmentComponent implements OnInit {
   id: number
   appointment: Appointment
@@ -18,5 +19,4 @@ export class DisplayAppointmentComponent implements OnInit {
     this.id = Number(this.route.snapshot.params['id'])
     this.appointment = this.appointmentService.getAppointments().filter(app => app.id === this.id)[0]
   }
-
 }
